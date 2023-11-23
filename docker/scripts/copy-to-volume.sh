7e320606983832f7a3e414466979fd8f3942ce06
Jens Petersohn <code@jen.pet>
2023-11-23 13:39:25 +0100

    fix: docker copy to volume now relies on docke cp

--- docker/scripts/copy-to-volume.sh
+++ docker/scripts/copy-to-volume.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 if [ -z "${VOLUME}" ] || [ -z "${VPATH}" ] || [ -z "${LPATH}" ]; then
-  echo "Missing 'VOLUME', 'VPATH' or 'lpath' parameter. Exiting (1)."
+  echo "Missing 'VOLUME', 'VPATH' or 'LPATH' parameter. Exiting (1)."
   exit 1
 fi
 
 echo "Ensuring docker volume existence of volume '${VOLUME}'..."
 docker volume create "${VOLUME}" >> /dev/null
 
+HELPER_CONTAINER_NAME="${HELPER_CONTAINER_NAME:-"little-helper"}"
+
 echo "Copying files from '${LPATH}' to '${VOLUME}:${VPATH}'..."
-docker run --rm --name="${HELPER_CONTAINER_NAME:-"little-helper"}" -v "${VOLUME}:/dest" -v "${LPATH}:/src" \
-  "${HELPER_IMAGE:-busybox}" /bin/sh -c "mkdir -p /dest/${VPATH} && cp -r /src/* /dest/${VPATH} \
-  && ls -lash /dest/${VPATH}"
-echo "Done copying."
+docker create --name="${HELPER_CONTAINER_NAME}" -v "${VOLUME}:/data/" busybox /bin/sh -c "ls -lash /data/${VPATH}" \
+  && docker cp "${LPATH}" "${HELPER_CONTAINER_NAME}:/data/${VPATH}" \
+  && docker start -a "${HELPER_CONTAINER_NAME}" \
+  && docker rm "${HELPER_CONTAINER_NAME}" > /dev/null \
+  && echo "Done copying."
