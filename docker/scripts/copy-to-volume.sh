93a4ae2bac98f0fdef03ada7ea478c7287b65941
Jens Petersohn <code@jen.pet>
2023-11-27 00:01:55 +0100

    feat: support owner and user when docker/ctv.

--- docker/scripts/copy-to-volume.sh
+++ docker/scripts/copy-to-volume.sh
@@ -10,8 +10,19 @@ docker volume create "${VOLUME}" >> /dev/null
 HELPER_CONTAINER_NAME="${HELPER_CONTAINER_NAME:-"little-helper"}"
 
 echo "Copying files from '${LPATH}' to '${VOLUME}:${VPATH}'..."
-docker create --name="${HELPER_CONTAINER_NAME}" -v "${VOLUME}:/data/" busybox /bin/sh -c "ls -lash /data/${VPATH}" \
-  && docker cp "${LPATH}" "${HELPER_CONTAINER_NAME}:/data/${VPATH}" \
-  && docker start -a "${HELPER_CONTAINER_NAME}" \
-  && docker rm "${HELPER_CONTAINER_NAME}" > /dev/null \
+docker run --name="${HELPER_CONTAINER_NAME}" -d -v "${VOLUME}:/data/" busybox sleep infinity > /dev/null \
+  && docker cp "${LPATH}" "${HELPER_CONTAINER_NAME}:/data/${VPATH}"
+
+if [ -n "${GROUP}" ]; then
+  docker exec "${HELPER_CONTAINER_NAME}" /bin/sh -c "chgrp -R ${GROUP} /data/${VPATH}"
+  echo "Set group to '${GROUP}'."
+fi
+
+if [ -n "${OWNER}" ]; then
+  docker exec "${HELPER_CONTAINER_NAME}" /bin/sh -c "chown -R ${OWNER} /data/${VPATH}"
+  echo "Set owner to '${OWNER}'."
+fi
+
+docker exec "${HELPER_CONTAINER_NAME}" /bin/sh -c "ls -lash /data/${VPATH}" \
+  && docker rm "${HELPER_CONTAINER_NAME}" -f > /dev/null \
   && echo "Done copying."
